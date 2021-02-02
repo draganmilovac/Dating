a4e73d78ec8fa5804bf925ead7bbdb7c2df26b8e
Dragan Milovac <d.milovac@rs.emakina.com>
2021-02-02 13:37:09 +0100

    Add client app

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,14 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +57,7 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -59,6 +68,8 @@ namespace API
             {
                 endpoints.MapControllers();
             });
+
+
         }
     }
 }
