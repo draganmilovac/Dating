ab1328fee4348491813c0c7fde3a8602a4b5d3d2
Dragan Milovac <d.milovac@rs.emakina.com>
2021-02-03 14:39:39 +0100

    Add initial Login

--- client-app/src/components/navbar.jsx
+++ client-app/src/components/navbar.jsx
@@ -1,29 +1,92 @@
-import React, { Component } from "react";
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
-import FormControl from 'react-bootstrap/FormControl';
-import Form from 'react-bootstrap/Form';
+import React, { Component, useEffect, useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 
-const NavBar = (props) => {
+const NavBar = () => {
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [isLogged, setIsLogged] = useState(false);
+
+  const logIn = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/user/login", user)
+      .then((response) => {
+        const u = response;
+        if (u) {
+          localStorage.setItem("token", u.data.token);
+          console.log(u.data.token);
+          setIsLogged(true);
+        }
+      });
+  };
+
+  const logOut = () => {
+    if (isLogged === true) {
+      localStorage.removeItem("token");
+      setIsLogged(false);
+    }
+  };
+
   return (
-<Navbar  bg="dark" variant="dark" expand="lg">
-  <Navbar.Brand href="#home">Dating App</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Matches</Nav.Link>
-      <Nav.Link href="#link">Lists</Nav.Link>
-      <Nav.Link href="#link">Messages</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Username" className="mr-sm-2" />
-      <FormControl type="text" placeholder="Password" className="mr-sm-2" />
-      <Button variant="outline-success">Login</Button>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Dating App</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Matches</Nav.Link>
+          <Nav.Link href="#link">Lists</Nav.Link>
+          <Nav.Link href="#link">Messages</Nav.Link>
+        </Nav>
+        <Form inline>
+          {!isLogged && (
+            <FormControl
+              type="text"
+              placeholder="Username"
+              className="mr-sm-2"
+              value={user.username}
+              onChange={(u) => {
+                setUser({ ...user, username: u.target.value });
+              }}
+            />
+          )}
+          {!isLogged && (
+            <FormControl
+              type="text"
+              placeholder="Password"
+              className="mr-sm-2"
+              value={user.password}
+              onChange={(u) => {
+                setUser({ ...user, password: u.target.value });
+              }}
+            />
+          )}
+          {isLogged && (
+            <Dropdown>
+              <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                {user.username}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Edit</Dropdown.Item>
+                <Dropdown.Item href="#/action-2" onClick={logOut}>Logout</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          )}
+          {!isLogged && (
+            <Button type="submit" variant="outline-success" onClick={logIn}>
+              Login
+            </Button>
+          )}
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
---
+++ client-app/src/shared/context/http-hook.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+const baseUrl = "http://localhost:5000";
+const HttpHook = props => {
+    return (  );
+}
+ 
+export default HttpHook;
